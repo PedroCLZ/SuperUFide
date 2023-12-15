9463ccf86148a8ea09fecb36a6d0097642352ffb
Francisco Rodriguez <franrodrisabo09@gmail.com>
2023-12-14 18:32:55 -0600

    Correcciones Login y Registro

--- ProyectoFinal/src/main/java/com/Proyecto/ProjectConfig.java
+++ ProyectoFinal/src/main/java/com/Proyecto/ProjectConfig.java
@@ -8,61 +8,21 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
 
 @Configuration
 public class ProjectConfig implements WebMvcConfigurer {
-    //Los siguientes métodos son utilizados para el tema de internacionalización
 
-    //LocaleResolver ubica la información del browser local del usuario y fija la información a desplegar
-    @Bean
-    public LocaleResolver localeResolver() {
-        var slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.getDefault());
-        slr.setLocaleAttributeName("session.current.locale");
-        slr.setTimeZoneAttributeName("session.current.timezone");
-        return slr;
-    }
-
-    //Define cual será la varible que "cambia" el idioma de los textos...
-    @Bean
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        var lci = new LocaleChangeInterceptor();
-        lci.setParamName("lang");
-        return lci;
-    }
-
-    //Se agrega un "interceptor" para poder hacer el cambio de idioma de inmediato...
-    @Override
-    public void addInterceptors(InterceptorRegistry registro) {
-        registro.addInterceptor(localeChangeInterceptor());
-    }
-
-    //Se utilizará para recuperar los texto dentro de java según el idioma...
-    @Bean("messageSource")
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource bundleMessageSource
-                = new ResourceBundleMessageSource();
-        bundleMessageSource.setBasename("messages");
-        bundleMessageSource.setDefaultEncoding("UTF-8");
-        return bundleMessageSource;
-    }
-
-    /* Los siguiente métodos son para implementar el tema de seguridad dentro del proyecto */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/index").setViewName("index");
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/registro/nuevo").setViewName("/registro/nuevo");
+        registry.addViewController("/producto/listado").setViewName("prodcutos");
     }
 
     @Bean
@@ -70,7 +30,7 @@ public class ProjectConfig implements WebMvcConfigurer {
         http
                 .authorizeHttpRequests((request) -> request
                 .requestMatchers("/", "/index", "/errores/**",
-                        "/carrito/**", "/pruebas/**", "/reportes/**",
+                        "/carrito/**", "/producto/**", 
                         "/registro/**", "/js/**", "/webjars/**")
                 .permitAll()
                 .requestMatchers(
